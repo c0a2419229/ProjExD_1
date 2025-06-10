e9c4984218f2f682efff4a9012ef19a704583dbd
c0a2419229 <c0a2419229@edu.teu.ac.jp>
2025-06-10 15:30:32 +0900

    2枚目反転

--- pygame_image.py
+++ pygame_image.py
@@ -10,6 +10,8 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg_img2 = pg.image.load("fig/pg_bg.jpg")
+    bg_img2 = pg.transform.flip(bg_img2, True, False)
     kok_img = pg.image.load("fig/3.png")
     kok_img = pg.transform.flip(kok_img, True, False)
     tmr = 0
@@ -18,7 +20,7 @@ def main():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [-tmr, 0])
-        screen.blit(bg_img, [-tmr+1600, 0])
+        screen.blit(bg_img2, [-tmr+1600, 0])
         screen.blit(kok_img, [300, 200])
         pg.display.update()
         tmr += 1        
